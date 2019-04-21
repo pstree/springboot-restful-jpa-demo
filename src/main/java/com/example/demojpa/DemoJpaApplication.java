b88506dff87c58ab75409fceec65f5a8b6f987b1
zhengqian <zhengqian@qq.com>
2019-04-21 22:32:12 +0800

    初始化

--- src/main/java/com/example/demojpa/DemoJpaApplication.java
+++ src/main/java/com/example/demojpa/DemoJpaApplication.java
@@ -1,13 +1,44 @@
 package com.example.demojpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableKafka
+@RestController
 @SpringBootApplication
 public class DemoJpaApplication {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoJpaApplication.class, args);
     }
 
+
+    @GetMapping("/")
+    public String ss() {
+        for (int i = 0; i < 10; i++) {
+            kafkaTemplate.send("springboot", "ssssss");
+        }
+        return "ss";
+    }
+
+    @KafkaListener(topics = "springboot",groupId = "1")
+    public void processMessage(String content) {
+        System.out.println("1111111111"+content);
+        //kafkaTemplate.send("springboot", "ssssss");
+    }
+
+    @KafkaListener(topics = "springboot",groupId = "1")
+    public void processMessage1(String content) {
+        System.out.println("2222222222"+content);
+        //kafkaTemplate.send("springboot", "ssssss");
+    }
 }
