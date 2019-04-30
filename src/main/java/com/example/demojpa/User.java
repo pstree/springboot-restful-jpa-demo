cea4548ec749b58af92394c2c1566dfa7fa6dba7
zhengqian <zhengqian@allinpay.com>
2019-04-30 12:51:32 +0800

    [优化]

--- src/main/java/com/example/demojpa/User.java
+++ src/main/java/com/example/demojpa/User.java
@@ -1,15 +1,26 @@
 package com.example.demojpa;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Where;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+
+//@ApiModel(value = "实体表")
 @Data
-@Entity(name = "t_sys_user")
+//@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "t_sys_user")
+@Where(clause = "is_delete != 1")
+@Cache(usage= CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, region="com.example.demojpa.User")
 public class User implements Serializable {
 
     @Id
