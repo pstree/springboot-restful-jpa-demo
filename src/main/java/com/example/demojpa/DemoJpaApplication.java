3d0b6e4a35a295d01788b82d2df699f7254c92cd
zhengqian <zhengqian@allinpay.com>
2019-09-05 17:24:15 +0800

    [增加]高级缓存 infinispan

--- src/main/java/com/example/demojpa/DemoJpaApplication.java
+++ src/main/java/com/example/demojpa/DemoJpaApplication.java
@@ -3,14 +3,12 @@ package com.example.demojpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableKafka
+@EnableCaching
 @RestController
 @SpringBootApplication
 public class DemoJpaApplication {
@@ -18,8 +16,7 @@ public class DemoJpaApplication {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoJpaApplication.class, args);
@@ -30,24 +27,4 @@ public class DemoJpaApplication {
         return userRepository.findById(id).orElse(null);
     }
 
-
-    @GetMapping("/")
-    public String ss() {
-        for (int i = 0; i < 10; i++) {
-            kafkaTemplate.send("springboot", "ssssss");
-        }
-        return "ss";
-    }
-
-    @KafkaListener(topics = "springboot",groupId = "1")
-    public void processMessage(String content) {
-        System.out.println("1111111111"+content);
-        //kafkaTemplate.send("springboot", "ssssss");
-    }
-
-    @KafkaListener(topics = "springboot",groupId = "1")
-    public void processMessage1(String content) {
-        System.out.println("2222222222"+content);
-        //kafkaTemplate.send("springboot", "ssssss");
-    }
 }
